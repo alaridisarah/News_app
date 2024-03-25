4791e3f8c8b6babad0e2e3c6ea2c7fd59985edaf
alaridisarah <92929752+alaridisarah@users.noreply.github.com>
2024-03-25 20:21:41 +0300

    home screen v1

--- news_app/lib/main.dart
+++ news_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/screens/home_screen.dart';
+import 'package:news_app/screens/notifications_screen.dart';
 
 void main() {
   runApp(const MyApp());
--- news_app/lib/screens/home_screen.dart
+++ news_app/lib/screens/home_screen.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:news_app/global_varible..dart';
+
+import 'items/bottomnav_bar.dart';
 
 class homescreen extends StatelessWidget {
   homescreen({super.key});
@@ -8,6 +12,10 @@ class homescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var _screenSize = MediaQuery.of(context).size;
     return Scaffold(
+      bottomNavigationBar: bottomnav(
+        index: 0,
+      ),
+      extendBodyBehindAppBar: true,
       body: SafeArea(
         child: Stack(children: [
           Column(children: [
@@ -23,16 +31,17 @@ class homescreen extends StatelessWidget {
                         controller: searchWord,
                         textAlignVertical: TextAlignVertical.center,
                         style: TextStyle(
-                            fontFamily: "Nunito",
-                            fontSize: 12,
-                            color: Color.fromARGB(255, 0, 0, 0)),
+                          fontFamily: "Nunito",
+                          fontSize: 12,
+                          color: Color.fromARGB(255, 0, 0, 0),
+                        ),
                         decoration: InputDecoration(
                           contentPadding: EdgeInsets.all(8),
                           suffixIcon: Icon(Icons.search),
                           hintText: '   Dogecoin to the Moon...',
                           border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(30))),
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                          ),
                         ),
                         onChanged: (value) {
                           // Handle text input changes
@@ -52,51 +61,131 @@ class homescreen extends StatelessWidget {
               ),
             ),
             Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          "Latest News",
-                          style: TextStyle(
-                              fontFamily: "Tinos",
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(width: _screenSize.width * 0.45),
-                        Row(
-                          children: [
-                            TextButton(
-                              child: Text(
-                                "See All",
-                                style: TextStyle(
-                                    fontFamily: "Nunito",
-                                    fontSize: 12,
-                                    color: Colors.blue),
-                              ),
-                              onPressed: () {},
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Latest News",
+                        style: TextStyle(
+                            fontFamily: "Tinos",
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(width: _screenSize.width * 0.45),
+                      Row(
+                        children: [
+                          TextButton(
+                            child: Text(
+                              "See All",
+                              style: TextStyle(
+                                  fontFamily: "Nunito",
+                                  fontSize: 12,
+                                  color: Colors.blue),
                             ),
-                            Icon(
-                              Icons.arrow_forward_rounded,
-                              color: Colors.blue,
-                              size: 15,
-                            )
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
-                )),
-            Row(
-              children: [
-                new_summary(
-                    "Crypto investors should be prepared to lose all their money, BOE governor says",
-                    "assets/news_image.png",
-                    "“I’m going to say this very bluntly again,” he added. “Buy them only if you’re prepared to lose all your money.”",
-                    _screenSize)
-              ],
-            )
+                            onPressed: () {},
+                          ),
+                          Icon(
+                            Icons.arrow_forward_rounded,
+                            color: Colors.blue,
+                            size: 15,
+                          )
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            // widget(
+            //   child:
+            SizedBox(
+              height: 250.0,
+              child: ListView.builder(
+                shrinkWrap: true,
+
+                scrollDirection: Axis.horizontal,
+                itemCount: 3, // number of articles,
+                itemBuilder: (context, index) {
+                  List articles = [
+                    new_summary(
+                        "Crypto investors should be prepared to lose all their money, BOE governor says",
+                        "assets/news_image.png",
+                        "“I’m going to say this very bluntly again,” he added. “Buy them only if you’re prepared to lose all your money.”",
+                        _screenSize),
+                    new_summary(
+                        "Crypto investors should be prepared to lose all their money, BOE governor says",
+                        "assets/news_image.png",
+                        "“I’m going to say this very bluntly again,” he added. “Buy them only if you’re prepared to lose all your money.”",
+                        _screenSize),
+                    new_summary(
+                        "Crypto investors should be prepared to lose all their money, BOE governor says",
+                        "assets/news_image.png",
+                        "“I’m going to say this very bluntly again,” he added. “Buy them only if you’re prepared to lose all your money.”",
+                        _screenSize)
+                  ];
+
+                  return articles[index];
+                },
+              ),
+            ),
+            SizedBox(height: 10),
+            SizedBox(
+              height: 40,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 6, // number of category,
+                itemBuilder: (context, index) {
+                  List category = [
+                    category_news("Health"),
+                    category_news("Art"),
+                    category_news("Sport"),
+                    category_news("Technology"),
+                    category_news("Science"),
+                    category_news("Lifestyle"),
+                  ];
+
+                  return category[index];
+                },
+              ),
+            ),
+            SizedBox(height: 10),
+            SizedBox(
+              height: 210,
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: 6, // number of category,
+                itemBuilder: (context, index) {
+                  List articles = [
+                    new_summary(
+                        "fvlwjebf;owefio;weio;wrgwegwgwegwe",
+                        "assets/news_image.png",
+                        "“fewpise all your money.”",
+                        _screenSize),
+                    new_summary("Crypto inrnor says", "assets/news_image.png",
+                        "“I’m go prepared to loseyour money.”", _screenSize),
+                    new_summary(
+                        "Crypto investors should be preparedr says",
+                        "assets/news_image.png",
+                        "“I’m going to say prepared to lomoney.”",
+                        _screenSize),
+                    new_summary(
+                        "Crypto investors should be preparedr says",
+                        "assets/news_image.png",
+                        "“I’m going to say prepared to lomoney.”",
+                        _screenSize),
+                    new_summary(
+                        "Crypto investors should be preparedr says",
+                        "assets/news_image.png",
+                        "“I’m going to say prepared to lomoney.”",
+                        _screenSize)
+                  ];
+
+                  return articles[index];
+                },
+              ),
+            ),
           ]),
         ]),
       ),
@@ -105,25 +194,58 @@ class homescreen extends StatelessWidget {
 
   Widget new_summary(
       String title, String new_image, String subtitle, var screen) {
-    return Container(
-      height: 100,
-      width: screen.width * 0.75,
-      child: Stack(children: [
-        Expanded(
-            child: Image.asset(new_image,
-                height: 100, width: screen.width * 0.75)),
-        Column(
-          children: [
-            Text(
-              title,
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 15),
-            ),
-            Text(subtitle,
-                style: TextStyle(fontSize: 10), textAlign: TextAlign.end)
-          ],
-        )
-      ]),
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Container(
+        height: 150,
+        width: screen.width * 0.55,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+              image: AssetImage(new_image),
+              fit: BoxFit.cover,
+            )),
+        child: Stack(children: [
+          //Image.asset(new_image, fit: BoxFit.cover),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                title,
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 12, color: Colors.white),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(subtitle,
+                  style: TextStyle(fontSize: 8, color: Colors.white),
+                  textAlign: TextAlign.end)
+            ],
+          )
+        ]),
+      ),
+    );
+  }
+
+  Widget category_news(String catrgory) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 15),
+      child: Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.pinkAccent,
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                catrgory,
+                style: TextStyle(fontSize: 12, color: Colors.white),
+              )
+            ],
+          )),
     );
   }
 }
